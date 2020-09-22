93aded6a390d67981c279866638ddd3fc503f63e
Ahmed Medra <ahmed.medrah@gmail.com>
2020-09-22 13:51:52 +0200

    Initial commit

---
+++ android/app/src/main/java/com/ahmedmedra/world_time/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ahmedmedra.world_time;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/Choose_Location.dart';
+import 'package:world_time/pages/Loading.dart';
+import 'package:world_time/pages/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/':(context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
+
+
